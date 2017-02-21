c4345e70a099907268b16a707c77ccc36d453b45
kevinch <ola@kevinch.com>
2017-02-21 15:14:04 -0300

    adding dynamic places

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      places: [
+        {
+          id: 1,
+          name: 'Pizza Storm',
+          location: 'Rua Maria Quiteria, 50'
+        },
+        {
+          id: 2,
+          name: 'Kebab',
+          location: 'Rua Sa Frereia, 150'
+        },
+        {
+          id: 3,
+          name: 'Pianense',
+          location: 'Rua Marques, 129'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>bora comer aonde?</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="add-place">
+          <label htmlFor="add-place-input">Add new</label>
+          <input id="add-place-input" type="text"/>
+        </div>
+        <div className="places-list">
+          <ul>
+            {this.state.places.map(place =>
+              <li key={place.id}>
+                <p className="place-name">{place.name}</p>
+                <p className="place-location">{place.location}</p>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     );
   }
